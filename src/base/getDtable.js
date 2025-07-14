a1be4525519c4ff521c72d52df7c000c69a52894
renjie-run <rj.aiyayao@gmail.com>
2025-07-14 10:27:09 +0800

    support get response headers

--- src/base/getDtable.js
+++ src/base/getDtable.js
@@ -3,5 +3,11 @@ const { auth } = require('../auth');
 (async () => {
   const base = await auth();
   const dtable = await base.getDTable();
+  console.log(base.getResponseHeaders());
+  console.log('------------------------------------------');
+  console.log(base.getResponseHeaders('x-ratelimit-limit'));
+  console.log(base.getResponseHeaders('x-ratelimit-remaining'));
+  console.log(base.getResponseHeaders('x-ratelimit-reset'));
+  console.log('------------------------------------------');
   console.log(dtable);
 })();
